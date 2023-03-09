63128d46f2854225d3c708b9e4e3f6ef890199fb
olgnite <lickotynf2@yandex.ru>
2023-03-09 15:01:50 +0500

    test

--- calorieCalculator/src/app/components/mortgage-calculator/mortgage-calculator.component.ts
+++ calorieCalculator/src/app/components/mortgage-calculator/mortgage-calculator.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-mortgage-calculator',
     templateUrl: './mortgage-calculator.component.html',
-    styleUrls: ['./mortgage-calculator.component.scss']
+    styleUrls: ['./mortgage-calculator.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MortgageCalculatorComponent implements OnInit {
 
